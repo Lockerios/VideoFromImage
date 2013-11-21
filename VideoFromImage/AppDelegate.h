21fd47d9a7e17ceedb56442c54bffef66fc30653
Lockerios <locklockzhang@gmail.com>
2013-11-21 09:04:18 +0800

    Initial Commit

---
+++ VideoFromImage/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  VideoFromImage
+//
+//  Created by Lockerios on 11/21/13.
+//  Copyright (c) 2013 Lockerios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ VideoFromImage/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  VideoFromImage
+//
+//  Created by Lockerios on 11/21/13.
+//  Copyright (c) 2013 Lockerios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
